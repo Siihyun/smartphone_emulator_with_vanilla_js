6178b6cdb01abece200a1024ac94fb6487f45b79
Sihyun <sihyun930222@gmail.com>
2021-08-23 06:53:25 +0900

    feat: add time display at the topbar
    화면 왼쪽 상단에 시간을 보여주는 time display를 추가하였습니다.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 import './styles.css';
+import { getFormatedTime } from './src/utils/getCurrentTime';
+
+const setTimeBar = () => {
+  const timeBar = document.querySelector('.time-bar');
+  setInterval(() => {
+    timeBar.textContent = getFormatedTime();
+  }, 1000);
+};
+
+setTimeBar();
---
+++ src/utils/getCurrentTime.js
@@ -0,0 +1,12 @@
+const getFormatedTime = () => {
+  const date = new Date();
+  const year = date.getFullYear();
+  const month = ('0' + (1 + date.getMonth())).slice(-2);
+  const day = ('0' + date.getDate()).slice(-2);
+  const hour = ('0' + date.getHours()).slice(-2);
+  const min = ('0' + date.getMinutes()).slice(-2);
+
+  return year + '/' + month + '/' + day + ' ' + hour + ':' + min;
+};
+
+export { getFormatedTime };
